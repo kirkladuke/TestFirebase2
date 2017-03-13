dc0ac7417321b814ab406aeaf51b5355c5726dd0
kirkladuke <ladukek1@ferris.edu>
2017-03-13 16:23:55 -0400

    3/13/2017
    added gmail sign in

--- app/src/main/java/com/example/kirk/testfirebase2/GoogleSignInActivity.java
+++ app/src/main/java/com/example/kirk/testfirebase2/GoogleSignInActivity.java
@@ -105,7 +105,7 @@ public class GoogleSignInActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        //  Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             if (result.isSuccess()) {
